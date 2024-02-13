5ca433e476032aea346a723de7b5c1bb3b3d9a47
Waleed0438 <i210438@nu.edu.pk>
2024-02-13 16:06:40 +0500

    Commited

---
+++ app/src/main/java/com/muhammadwaleed/i210438/Mentorhome.kt
@@ -0,0 +1,9 @@
+package com.muhammadwaleed.i210438
+
+data class Mentorhome(
+    val name: String,
+    val occupation: String,
+    val status: String,
+    val pricePerSession: String,
+    val isFavorite: Boolean
+)
