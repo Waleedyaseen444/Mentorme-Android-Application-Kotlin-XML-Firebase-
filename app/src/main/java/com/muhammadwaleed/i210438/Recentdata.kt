47e56ea227d8530340cda04dc0f0e2390146edd5
Waleed0438 <i210438@nu.edu.pk>
2024-02-13 19:04:33 +0500

    Commited

---
+++ app/src/main/java/com/muhammadwaleed/i210438/Recentdata.kt
@@ -0,0 +1,10 @@
+package com.muhammadwaleed.i210438
+
+data class Recentdata(
+    val sampleText: String,
+    val occupationText: String,
+    val statusText: String,
+    val priceText: String,
+    val profileIconRes: Int,
+    val heartIconRes: Int
+)
