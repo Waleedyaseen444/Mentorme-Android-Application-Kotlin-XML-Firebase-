a4bf8d966d2a8e1bfee4dac71b617c69dea9ca33
Waleed0438 <i210438@nu.edu.pk>
2024-02-13 11:13:32 +0500

    Commited

--- app/src/main/java/com/muhammadwaleed/i210438/Editprofile.kt
+++ app/src/main/java/com/muhammadwaleed/i210438/Editprofile.kt
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 
 class Editprofile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editprofile)
-        val UpdateBtn = findViewById<Button>(R.id.IdUpdate)
-        UpdateBtn.setOnClickListener {
+
+        val updateBtn = findViewById<Button>(R.id.IdUpdate)
+        updateBtn.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
-            }
+        }
+
+        val back = findViewById<ImageButton>(R.id.backbutton)
+        back.setOnClickListener {
+            // Navigate back to the previous activity
+            onBackPressed()
         }
     }
+}
