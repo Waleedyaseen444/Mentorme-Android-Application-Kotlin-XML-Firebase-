cfad8c8177de3ce2277ce4e40fa07f5e5da73670
Waleed0438 <i210438@nu.edu.pk>
2024-02-13 16:06:14 +0500

    Commited

---
+++ app/src/main/java/com/muhammadwaleed/i210438/EducationMentor.kt
@@ -0,0 +1,9 @@
+package com.muhammadwaleed.i210438
+
+data class EducationMentor(
+    val name: String,
+    val profession: String,
+    val status: String,
+    val pricePerSession: String,
+    val isFavorite: Boolean
+)
