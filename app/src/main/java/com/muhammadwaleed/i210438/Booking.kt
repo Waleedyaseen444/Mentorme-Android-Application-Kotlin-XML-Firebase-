ea16fec6e3256d813038e1b1fe0f33eab786ddfe
Waleed0438 <i210438@nu.edu.pk>
2024-03-20 22:44:02 +0500

    Commited

--- app/src/main/java/com/muhammadwaleed/i210438/Booking.kt
+++ app/src/main/java/com/muhammadwaleed/i210438/Booking.kt
@@ -1,27 +1,131 @@
 package com.muhammadwaleed.i210438
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ServerValue
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Booking : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var selectedDate: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking)
 
         val backButton = findViewById<ImageButton>(R.id.imageButton14)
         val bookAppointmentButton = findViewById<Button>(R.id.button5)
+        val nameTextView = findViewById<TextView>(R.id.textView46)
+        val professionTextView = findViewById<TextView>(R.id.textView63)
+        val mentorImageView = findViewById<CircleImageView>(R.id.textView12)
+        val calendarView = findViewById<CalendarView>(R.id.calendarView)
 
-        backButton.setOnClickListener {
-            val intent = Intent(this, Mentordetails::class.java)
-            startActivity(intent)
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        // Receive mentor data from intent
+        val mentorName = intent.getStringExtra("MENTOR_NAME")
+        val mentorProfession = intent.getStringExtra("MENTOR_DESCRIPTION")
+        val mentorImageUrl = intent.getStringExtra("MENTOR_IMAGE_URL")
+
+        val image2 = findViewById<ImageButton>(R.id.imageButton2)
+
+
+        image2.setOnClickListener {
+            val user = auth.currentUser
+            if (user != null) {
+                val intent = Intent(this@Booking, Chatopen::class.java)
+                intent.putExtra("MENTOR_NAME", mentorName)
+                startActivity(intent)
+            } else {
+                // User is not signed in, handle this case as needed
+            }
+        }
+        // Set received mentor data to TextViews
+        nameTextView.text = mentorName
+        professionTextView.text = mentorProfession
+
+        // Load mentor image using Picasso
+        if (!mentorImageUrl.isNullOrEmpty()) {
+            Picasso.get().load(mentorImageUrl).into(mentorImageView)
         }
 
+        backButton.setOnClickListener {
+            onBackPressed()
+        }
+
+        // On calendar date change, update the selected date
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val calendar = Calendar.getInstance()
+            calendar.set(year, month, dayOfMonth)
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            selectedDate = sdf.format(calendar.time)
+        }
+
+        // On button click, save booking details to Firebase Realtime Database
         bookAppointmentButton.setOnClickListener {
-            val intent = Intent(this, MainActivity3::class.java)
-            startActivity(intent)
+            val user = auth.currentUser
+            if (user != null) {
+                if (selectedDate.isNullOrEmpty()) {
+                    Toast.makeText(this, "Please select a date", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+
+                // Get current time
+                val calendar = Calendar.getInstance()
+                val currentTime = SimpleDateFormat("HH:mm", Locale.getDefault()).format(calendar.time)
+
+                // Create a reference to the Firebase database
+                val database = FirebaseDatabase.getInstance()
+                val userId = user.uid
+                val myRef = database.getReference("users/$userId/booked_sessions")
+
+                // Generate a unique key for the session
+                val sessionKey = myRef.push().key
+
+                // Create a Session object
+                val session = SessionDetails(
+                    mentorName ?: "",
+                    mentorProfession ?: "",
+                    selectedDate,
+                    currentTime,
+                    mentorImageUrl ?: ""
+                )
+
+                // Save the session to the database under the generated key
+                if (sessionKey != null) {
+                    myRef.child(sessionKey).setValue(session)
+                        .addOnSuccessListener {
+
+                            val notificationRef = FirebaseDatabase.getInstance().reference.child("notifications").push()
+                            val notificationMessage = "Booking made for mentor: $mentorName"
+                            val notification = mapOf(
+                                "message" to notificationMessage,
+                                "timestamp" to ServerValue.TIMESTAMP
+                            )
+                            notificationRef.setValue(notification)
+                            Toast.makeText(this, "Session Booked Successfully!", Toast.LENGTH_SHORT).show()
+                            // After booking, navigate to Bookedsession activity
+                            startActivity(intent)
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(this, "Failed to book session.", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            } else {
+                // User is not signed in, handle this case as needed
+            }
         }
     }
 }
--- app/src/main/java/com/muhammadwaleed/i210438/Call.kt
+++ app/src/main/java/com/muhammadwaleed/i210438/Call.kt
@@ -1,26 +1,88 @@
 package com.muhammadwaleed.i210438
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import io.agora.rtc2.Constants
+import io.agora.rtc2.Constants.LOG_FILTER_DEBUG
+import io.agora.rtc2.IRtcEngineEventHandler
+import io.agora.rtc2.RtcEngine
+import io.agora.rtc2.video.VideoCanvas
+import io.agora.rtc2.video.VideoEncoderConfiguration
 
 class Call : AppCompatActivity() {
+    private lateinit var rtcEngine: RtcEngine
+    private val agoraAppId = "68c966940e81407e904559670866f92d"
+    private val agoraToken = "007eJxTYNi6XXX6ezPX5sJDZ/nnXj/lbfr25BklnYI/nZMvxVz58dtTgcHMItnSzMzSxCDVwtDEwDzV0sDE1NTSzNzAwswszdIoheHp19SGQEaG5xK8jIwMEAjiczKU5WcmpyYn5uQwMAAAnn4i/w=="
+    private val channelName = "voicecall"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call)
 
-        val source = intent.getStringExtra("source")
+        rtcEngine = RtcEngine.create(this, agoraAppId, object : IRtcEngineEventHandler() {
+            override fun onUserJoined(uid: Int, elapsed: Int) {
+                runOnUiThread {
+                    setupRemoteVideo(uid)
+                }
+            }
+        })
+
+        rtcEngine.setLogFilter(LOG_FILTER_DEBUG)
+        rtcEngine.setLogFile("/path/to/logfile.log")
 
         val imageButton24 = findViewById<ImageButton>(R.id.imageButton24)
         imageButton24.setOnClickListener {
-            if (source == "Chatopen") {
-                val intent = Intent(this, Chatopen::class.java)
-                startActivity(intent)
-            } else if (source == "Communitychat") {
-                val intent = Intent(this, Communitychat::class.java)
-                startActivity(intent)
-            }
+            joinChannel()
         }
     }
+
+    private fun joinChannel() {
+        rtcEngine.setChannelProfile(Constants.CHANNEL_PROFILE_COMMUNICATION)
+        rtcEngine.enableVideo()
+
+        rtcEngine.setVideoEncoderConfiguration(
+            VideoEncoderConfiguration(
+                VideoEncoderConfiguration.VD_640x360,
+                VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_15,
+                VideoEncoderConfiguration.STANDARD_BITRATE,
+                VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_ADAPTIVE
+            )
+        )
+
+        rtcEngine.joinChannel(agoraToken, channelName, "", 0)
+
+        val localSurfaceView = RtcEngine.CreateRendererView(this)
+        localSurfaceView.setZOrderMediaOverlay(true)
+        val container = findViewById<FrameLayout>(R.id.local_video_view_container)
+        container.addView(localSurfaceView)
+        rtcEngine.setupLocalVideo(VideoCanvas(localSurfaceView, VideoCanvas.RENDER_MODE_HIDDEN, 0))
+
+        val remoteSurfaceView = RtcEngine.CreateRendererView(this)
+        remoteSurfaceView.setZOrderMediaOverlay(true)
+        val remoteContainer = findViewById<FrameLayout>(R.id.remote_video_view_container)
+        remoteContainer.addView(remoteSurfaceView)
+        rtcEngine.setupRemoteVideo(VideoCanvas(remoteSurfaceView, VideoCanvas.RENDER_MODE_HIDDEN, 0))
+
+        rtcEngine.setEnableSpeakerphone(true) // Enable speakerphone
+    }
+
+    private fun setupRemoteVideo(uid: Int) {
+        val remoteSurfaceView = RtcEngine.CreateRendererView(this)
+        remoteSurfaceView.setZOrderMediaOverlay(true)
+        val remoteContainer = findViewById<FrameLayout>(R.id.remote_video_view_container)
+        remoteContainer.addView(remoteSurfaceView)
+        rtcEngine.setupRemoteVideo(VideoCanvas(remoteSurfaceView, VideoCanvas.RENDER_MODE_FIT, uid))
+    }
+
+    override fun onDestroy() {
+        rtcEngine.leaveChannel()
+        rtcEngine.destroy()
+        super.onDestroy()
+    }
+}
+
+private fun RtcEngine.destroy() {
+    TODO("Not yet implemented")
 }
